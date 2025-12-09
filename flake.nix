de62b82f5729166ae191b7b516fa9e4ec30b9771
Emory Merryman <emory.merryman@gmail.com>
2025-12-09 13:17:28 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -44,7 +44,8 @@
                                                                                         MESSAGE="$*"
                                                                                         UUID="$( uuidgen )" || failure 5fa834fd
                                                                                         BRANCH="$( echo "issue/$UUID" | cut --characters 1-64 )" || failure 5deab959
-                                                                                        SNAPSHOT=${ private ( setup : ''${ setup } $BRANCH" "$MESSAGE"'' ) }
+                                                                                        PRIVATE=${ private ( setup : ''${ setup } $BRANCH" "$MESSAGE"'' ) }
+                                                                                        export PRIVATE
                                                                                     '' ;
                                                                     }
                                                             )
